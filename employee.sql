cf33b3d04a24c3117ce3a22026e37d596956ef82
rashawn doyley <rashawndoyley@gmail.com>
2021-03-11 14:55:18 -0500

    getting all my functions up to par

--- employee.sql
+++ employee.sql
@@ -1,24 +1,27 @@
 DROP DATABASE IF EXISTS employeeDB;
 CREATE DATABASE employeeDB;
 USE employeeDB;
+
 CREATE TABLE department (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR (30) UNIQUE NOT NULL
+    PRIMARY KEY (id)
 )
 CREATE TABLE role (
     department_id INT NOT NULL UNSIGNED,
-    INDEX dep_id (department_id),
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR (30),
     salary INT NOT NULL UNSIGNED
+    PRIMARY KEY (id)
+    FOREIGN key (department_id) REFERENCES department(id)
 )
 CREATE TABLE employee (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR (30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
-    role_id INT NOT NULL UNSIGNED,
-    INDEX role_id (role_id),
+    role_id INT NOT NULL ,
     manager_id INT UNSIGNED,
-    INDEX man_id (manager_id)
-    FOREIGN KEY (employee_id) REFERENCES role_id(role_id)
+    PRIMARY KEY (id)
+    FOREIGN KEY (role_id) REFERENCES role_id(role_id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 )
-constraint on FOREIGN keys 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const consoleTable = require("console.table");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -14,24 +15,12 @@ const connection = mysql.createConnection({
     password: "Babytroy12",
     database: "employeeDB",
 });
-const createEmployee = (response) => {
-    console.log("Inserting a new employee...\n");
-    const query = connection.query(
-        "INSERT INTO employee SET ?", {
-            first_name: response.username,
-            last_name: response.salary,
-            role_id: response.role,
-        },
-        (err, res) => {
-            console.log(res);
-            if (err) throw err;
-
-            console.log(`${res.affectedRows} product inserted!\n`);
-            // Call updateProduct AFTER the INSERT completes
-            // updateProduct();
-        }
-    );
-};
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    // roles();
+    main();
+});
 
 function getEmployees() {
     inquirer
@@ -59,8 +48,11 @@ function getEmployees() {
 
 function main() {
     inquirer
-        .prompt([
-            [
+        .prompt({
+            name: "mainprompt",
+            type: "list",
+            message: "Choose from the following below",
+            choices: [
                 "View all employees",
                 "View all employees by role",
                 "View all employees by department",
@@ -69,25 +61,110 @@ function main() {
                 "Add role",
                 "Add department",
             ],
-        ])
+        })
         .then((ans) => {
-            switch (ans.action) {
+            switch (ans.mainprompt) {
                 case "View all employees":
                     viewAllEmp();
                     break;
                 case "View all employees by role":
+                    viewAllByRoles();
                     break;
                 case "View all employees by department":
+                    viewAllByDepartment();
                     break;
                 case "View all employees by manager":
+                    viewAllByManager();
                     break;
                 case "Add employee":
+                    addEmp();
+                    break;
+                case "Add role":
+                    addRole();
+                    break;
+                case "Add department":
+                    addDept();
                     break;
             }
         });
 }
-connection.connect((err) => {
-    if (err) throw err;
-    console.log(`connected as id ${connection.threadId}`);
-    getEmployees();
-});
+
+function viewAllEmp() {
+    // construct our sql query
+    let sql =
+        " SELECT emp.id, emp.first_name, emp.last_name, role.title, department.name department, role.salary, concat(m.first_name, ' ', m.last_name) manager FROM employee emp LEFT JOIN employee m ON emp.manager_id = m.id INNER JOIN role ON emp.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC ";
+    // make query
+    connection.query(sql, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        main();
+    });
+}
+
+function roles() {
+    let roleSet = [];
+    let sql = " SELECT * FROM role";
+    connection.query(sql, (err, res) => {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            roleSet.push(res[i].title);
+        }
+    });
+    return roleSet;
+}
+
+function department() {
+    let deptSet = [];
+    let sql = " SELECT * FROM department";
+    connection.query(sql, (err, res) => {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            deptSet.push(res[i].title);
+        }
+    });
+    return roleSet;
+}
+
+function viewAllByRoles() {
+    let sql =
+        "SELECT emp.id,emp.first_name, emp.last_name,role.title FROM employee emp INNER JOIN role on emp.role_id = role.id ";
+    connection.query(sql, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        main();
+    });
+}
+
+function viewAllByDepartment() {}
+
+function viewAllByManager() {}
+
+function addDept() {}
+
+function addRole() {}
+
+function addEmp() {
+    inquirer.prompt([{
+            name: "firstName",
+            type: "input",
+            message: "please input First Name",
+        },
+        {
+            name: "lastName",
+            type: "input",
+            message: "please input Last Name",
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "What is the role",
+            choices: roles(),
+        },
+        {
+            name: "dept",
+            type: "list",
+            message: "What is the department that you are assigned to",
+            choices: department(),
+        },
+    ]);
+}
